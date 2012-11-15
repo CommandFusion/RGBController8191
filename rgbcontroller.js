671f48526e71b69a0e30cceb7406c205056ac1e5
Jarrod Bell <jarrod@commandfusion.com>
2012-11-16 03:43:38 +1100

    Added bezier curve implementation to change light intensity curve for more even control of levels.
    Signed-off-by: Jarrod Bell <jarrod@commandfusion.com>

--- rgbcontroller.js
+++ rgbcontroller.js
@@ -4,7 +4,9 @@ var RGBController = function(params) {
 	var self = {
 		prefix: 0xFE,
 		address: params.address || 0x4C,
-		systemName: params.systemName || "RGBController"
+		systemName: params.systemName || "RGBController",
+		k : new KeySpline(0.96,0.2,0.9,0.69) // This is used to change the curve of light intensity - LEDs intensity change is much more noticable at bottom end of the scale
+		// Use the live demo tool on this page to change the curve values for the above KeySpline function: http://blog.greweb.fr/2012/02/bezier-curve-based-easing-functions-from-concept-to-implementation/
 	};
 
 	self.setAddress = function(newAddress) {
@@ -21,7 +23,9 @@ var RGBController = function(params) {
 		} else if (level > 255) {
 			level = 255;
 		}
-		self.sendData(0x42, channel, parseInt(level/1.02));
+		var time = (1/250) * (level/1.02);
+		var newLevel = parseInt(250 * self.k.get(time));
+		self.sendData(0x42, channel, newLevel);
 	};
 
 	self.setAllLevels = function(level) {
@@ -69,3 +73,44 @@ CF.modules.push({
 	object: RGBController,	// the object to which the setup function belongs ("this")
 	version: 1.0	// An optional module version number that is displayed in the Remote Debugger
 });
+
+/**
+* KeySpline - use bezier curve for transition easing function
+* is inspired from Firefox's nsSMILKeySpline.cpp
+* Usage:
+* var spline = new KeySpline(0.25, 0.1, 0.25, 1.0)
+* spline.get(x) => returns the easing value | x must be in [0, 1] range
+*/
+function KeySpline (mX1, mY1, mX2, mY2) {
+
+  this.get = function(aX) {
+    if (mX1 == mY1 && mX2 == mY2) return aX; // linear
+    return CalcBezier(GetTForX(aX), mY1, mY2);
+  }
+
+  function A(aA1, aA2) { return 1.0 - 3.0 * aA2 + 3.0 * aA1; }
+  function B(aA1, aA2) { return 3.0 * aA2 - 6.0 * aA1; }
+  function C(aA1)      { return 3.0 * aA1; }
+
+  // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
+  function CalcBezier(aT, aA1, aA2) {
+    return ((A(aA1, aA2)*aT + B(aA1, aA2))*aT + C(aA1))*aT;
+  }
+
+  // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
+  function GetSlope(aT, aA1, aA2) {
+    return 3.0 * A(aA1, aA2)*aT*aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
+  }
+
+  function GetTForX(aX) {
+    // Newton raphson iteration
+    var aGuessT = aX;
+    for (var i = 0; i < 4; ++i) {
+      var currentSlope = GetSlope(aGuessT, mX1, mX2);
+      if (currentSlope == 0.0) return aGuessT;
+      var currentX = CalcBezier(aGuessT, mX1, mX2) - aX;
+      aGuessT -= currentX / currentSlope;
+    }
+    return aGuessT;
+  }
+}
